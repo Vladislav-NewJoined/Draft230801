26255e8571bada6db066a1c683efdcf1a96d04a2
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-03 14:16:51 +0300

    Initial commit

--- src/Draft_Task1_3_6_1.java
+++ src/Draft_Task1_3_6_1.java
@@ -1,5 +1,7 @@
-public class Draft_Whole_Video1_3_6_0 {
+public class Draft_Task1_3_6_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание: \n1.  Пользователь вводит 10 слов в массив. Найдите первое слово, " +
+                "\nв котором есть две гласные буквы подряд\n\nРешение: ");
+
     }
 }
--- src/Draft_Whole_Video1_3_6_0.java
+++ src/Draft_Whole_Video1_3_6_0.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class Draft_Whole_Video1_3_6_0 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //  Урок 6. Операторы Continue, break
+        //  видео мин 03 01 - найти певый элемент массива, кот. больше 10-ти
+
+        int[] arr = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array of 5 elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int x = 0;
+        while (x < 100) {
+            if (x == 50) {
+                break;
+            }
+            x++;
+            System.out.println(x);
+        }
+        System.out.println("end of program");
+
+
     }
 }
