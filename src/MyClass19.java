80d5cc09a71131873ae9100c7460ff382e46844f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-04 10:24:57 +0300

    Initial commit

--- src/MyClass19.java
+++ src/MyClass19.java
@@ -4,24 +4,52 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class MyClass19 {   //  itunes ДжейСон JSON_2
     public static void main(String[] args) throws IOException {
-        System.out.println("What you looking for in itunes?");
-//        String url = "https://itunes.apple.com/search?term=never+let+you+go&limit=1"; //  32 41
-        // JSON BEAUTYFY  https://codebeautify.org/jsonviewer
-//        String url = "https://itunes.apple.com/search?term=war+of+worlds&limit=1"; //  32 41
-        String url = "https://itunes.apple.com/search?term=never+let+you+go&limit=1"; //  32 41
-//        String url = "https://itunes.apple.com/search?term=Brad+Pitt&limit=1"; //  32 41
-        String page = downloadWebPage(url);
-        System.out.println(page);
-        int start = page.indexOf("kind");
-        int end = page.indexOf("\",", start);  //  ищем не с самого начала, а после start
-        String sub =  page.substring(start + 7, end);
-        System.out.println(page.substring(start + 7, end));
-    }
-    // Закончил: видео мин 42 46 https://lms.synergy.ru/student/updiscipline/4474947/1045153/1/1
 
+        // userInput
+        String term = getUserInput();
+
+        // buildUrl
+        String url = buildUrl(term);
+
+        // downloadWebPage
+        String page = downloadWebPage(url);
+
+        // parseResult
+        printResult(page);
+        System.out.println(page); // целиком всю страницу не будем выводить
+
+    }
+    // Закончил: видео мин 42 46  (1-3_2) https://lms.synergy.ru/student/updiscipline/4474947/1045153/1/1
+
+        static void/*String*/ /*parseResult*/printResult(String page) {
+            // parseResult
+//            int start = page.indexOf("kind") + 7;  // было /*kind*/
+            int start = page.indexOf("wrapperType") + 14;  // было /*kind*/
+            int end = page.indexOf("\",", start); //  ищем не с самого начала, а после start
+//        String sub =  page.substring(start + 7, end);  // это я дописал
+            System.out.println(page.substring(start, end));
+        }
+
+        static String/*void*/ buildUrl (String partOfRequest) {
+            // buildUrl
+            String termWithoutSpaces = /*term*/partOfRequest.replaceAll(" ", "+");
+            String itunesApi = "https://itunes.apple.com/search?term="; //  32 41
+            String limitParametr = "&limit=1";
+            String url = itunesApi + termWithoutSpaces + limitParametr; //  32 41
+            return url;
+        }
+
+        static String/*void*/ getUserInput() {
+            // userInput
+            System.out.println("What you looking for in itunes?");
+            Scanner scanner = new Scanner(System.in);
+            String info /*term*/ = scanner.nextLine(); //  war of worlds
+            return info;
+        }
 
 
 
