d0078da028faeaddbface5c53765da49b0129b76
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-18 20:22:32 +0300

    Initial commit

--- src/Draft_Task1_3_8_4.java
+++ src/Draft_Task1_3_8_4.java
@@ -10,6 +10,44 @@ public class Draft_Task1_3_8_4 {
 
                 Решение:\s""");
 
+        int[] array1 = new int[10];
+        array1[0] = 5;
+        array1[1] = 3;
+        array1[2] = 5;
+        array1[3] = 3;
+        array1[4] = 4;
+        array1[5] = 4;
+        array1[6] = 3;
+        array1[7] = 2;
+        array1[8] = 5;
+        array1[9] = 4;
+
+        int[] array2 = new int[10];
+        array2[0] = 7;
+        array2[1] = 2;
+        array2[2] = 5;
+        array2[3] = 1;
+        array2[4] = 12;
+        array2[5] = 56;
+        array2[6] = 6;
+        array2[7] = 1;
+        array2[8] = 33;
+        array2[9] = 2;
+
+        int[] array3 = new int[10];
+        array3[0] = 6;
+        array3[1] = 8;
+        array3[2] = 12;
+        array3[3] = 101;
+        array3[4] = 22;
+        array3[5] = 3;
+        array3[6] = 8;
+        array3[7] = 0;
+        array3[8] = 9;
+        array3[9] = 7;
+
+        int[] array4 = new int[20];
+
     }
 }
 //        Конец Примера _ КККККККККККККККК
