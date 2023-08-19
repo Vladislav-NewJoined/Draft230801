8f883b632d3bf2c00362a1951bdf5b177f466ea6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-19 20:15:38 +0300

    Initial commit

--- src/Draft_Task1_3_8_4.java
+++ src/Draft_Task1_3_8_4.java
@@ -1,57 +1,193 @@
 import java.util.Arrays;
 
-//        Пример _ ППППППППППППППППППППППППППППППППППП  //
+//   ПРИВЕТ. ТЕСТИРУЕМ!
+//        Пример _ ППППППППППППППППППППППППППППППППППП  //  из интернет: 
 public class Draft_Task1_3_8_4 {
     public static void main(String[] args) {
 //  Пример взят отсюда: видео мин 06 15 https://youtu.be/tAtBPxw2RRQ   https://yandex.ru/video/preview/2055233165764642050
         System.out.println("""
                 Задание:\s
-                4. Реализуйте методы merge(int[] array1, int[] array2), merge(int[] array1, int[]
-                   array2,int[] array3), merge(int[] array1, int[] array2, int[] array3, int[] array4) -
+                4. Реализуйте методы merge(int[] array1, int[] arr2), merge(int[] array1, int[]
+                   arr2,int[] arr3), merge(int[] array1, int[] arr2, int[] arr3, int[] array4) -
                    который возвращает новый массив, в котором он соединяет все предыдущие
                    (было 3 массива по 10 элементов, станет массив с 20 элементами)
 
                 Решение:\s""");
 
-        int[] array1 = new int[10];
-        array1[0] = 5;
-        array1[1] = 3;
-        array1[2] = 5;
-        array1[3] = 3;
-        array1[4] = 4;
-        array1[5] = 4;
-        array1[6] = 3;
-        array1[7] = 2;
-        array1[8] = 5;
-        array1[9] = 4;
+        int[] arr1 = new int[10];
+        arr1[0] = 0;
+        arr1[1] = 1;
+        arr1[2] = 2;
+        arr1[3] = 3;
+        arr1[4] = 4;
+        arr1[5] = 5;
+        arr1[6] = 6;
+        arr1[7] = 7;
+        arr1[8] = 8;
+        arr1[9] = 9;
 
-        int[] array2 = new int[10];
-        array2[0] = 7;
-        array2[1] = 2;
-        array2[2] = 5;
-        array2[3] = 1;
-        array2[4] = 12;
-        array2[5] = 56;
-        array2[6] = 6;
-        array2[7] = 1;
-        array2[8] = 33;
-        array2[9] = 2;
+        int[] arr2 = new int[10];
+        arr2[0] = 10;
+        arr2[1] = 20;
+        arr2[2] = 30;
+        arr2[3] = 40;
+        arr2[4] = 50;
+        arr2[5] = 60;
+        arr2[6] = 70;
+        arr2[7] = 80;
+        arr2[8] = 90;
+        arr2[9] = 100;
 
-        int[] array3 = new int[10];
-        array3[0] = 6;
-        array3[1] = 8;
-        array3[2] = 12;
-        array3[3] = 101;
-        array3[4] = 22;
-        array3[5] = 3;
-        array3[6] = 8;
-        array3[7] = 0;
-        array3[8] = 9;
-        array3[9] = 7;
+        int[] arr3 = new int[10];
+        arr3[0] = 5;
+        arr3[1] = 15;
+        arr3[2] = 25;
+        arr3[3] = 35;
+        arr3[4] = 45;
+        arr3[5] = 55;
+        arr3[6] = 65;
+        arr3[7] = 75;
+        arr3[8] = 85;
+        arr3[9] = 95;
 
         int[] array4 = new int[20];
 
+        int[] srr = merge(arr1, arr2);
+        System.out.println(Arrays.toString(srr));
+    }
+
+    public static int[] merge(int arr1[], int arr2[]) {
+        int[] arr1_and_arr2 = new int[arr1.length + arr2.length];
+        int n = arr1.length;
+        int m = arr2.length;
+        int i = 0, j = 0, k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                arr1_and_arr2[k] = arr1[i];
+                i++;
+            } else {
+                arr1_and_arr2[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n) {
+            arr1_and_arr2[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < m) {
+            arr1_and_arr2[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return arr1_and_arr2;
 
     }
 }
 //        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 1 ППППППППППППППППППППППППППППППППППП  //  видео из интернет, отсюда: https://youtu.be/tAtBPxw2RRQ
+//public class Draft_Task1_3_8_4 {
+//    public static void main(String[] args) {
+////  Пример взят отсюда: видео мин 06 15 https://youtu.be/tAtBPxw2RRQ   https://yandex.ru/video/preview/2055233165764642050
+//        System.out.println("""
+//                Задание:\s
+//                4. Реализуйте методы merge(int[] array1, int[] arr2), merge(int[] array1, int[]
+//                   arr2,int[] arr3), merge(int[] array1, int[] arr2, int[] arr3, int[] array4) -
+//                   который возвращает новый массив, в котором он соединяет все предыдущие
+//                   (было 3 массива по 10 элементов, станет массив с 20 элементами)
+//
+//                Решение:\s""");
+//
+//        int[] arr1 = new int[10];
+//        arr1[0] = 0;
+//        arr1[1] = 1;
+//        arr1[2] = 2;
+//        arr1[3] = 3;
+//        arr1[4] = 4;
+//        arr1[5] = 5;
+//        arr1[6] = 6;
+//        arr1[7] = 7;
+//        arr1[8] = 8;
+//        arr1[9] = 9;
+//
+//        int[] arr2 = new int[10];
+//        arr2[0] = 10;
+//        arr2[1] = 20;
+//        arr2[2] = 30;
+//        arr2[3] = 40;
+//        arr2[4] = 50;
+//        arr2[5] = 60;
+//        arr2[6] = 70;
+//        arr2[7] = 80;
+//        arr2[8] = 90;
+//        arr2[9] = 100;
+//
+//        int[] arr3 = new int[10];
+//        arr3[0] = 5;
+//        arr3[1] = 15;
+//        arr3[2] = 25;
+//        arr3[3] = 35;
+//        arr3[4] = 45;
+//        arr3[5] = 55;
+//        arr3[6] = 65;
+//        arr3[7] = 75;
+//        arr3[8] = 85;
+//        arr3[9] = 95;
+//
+//        int[] array4 = new int[20];
+//
+//        int[] srr = merge(arr1, arr2);
+//        System.out.println(Arrays.toString(srr));
+//    }
+//
+//    public static int[] merge(int arr1[], int arr2[]) {
+//        int[] arr1_and_arr2 = new int[arr1.length + arr2.length];
+//        int n = arr1.length;
+//        int m = arr2.length;
+//        int i = 0, j = 0, k = 0;
+//
+//        while (i < n && j < m) {
+//            if (arr1[i] <= arr2[j]) {
+//                arr1_and_arr2[k] = arr1[i];
+//                i++;
+//            } else {
+//                arr1_and_arr2[k] = arr2[j];
+//                j++;
+//            }
+//            k++;
+//        }
+//        while (i < n) {
+//            arr1_and_arr2[k] = arr1[i];
+//            i++;
+//            k++;
+//        }
+//        while (j < m) {
+//            arr1_and_arr2[k] = arr2[j];
+//            j++;
+//            k++;
+//        }
+//
+//        return arr1_and_arr2;
+//
+//    }
+//}
+////        Конец Примера 1 КККККККККККККККК
+
+
+
+
+
+
+
+
