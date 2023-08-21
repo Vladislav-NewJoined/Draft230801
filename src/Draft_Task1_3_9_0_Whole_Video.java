d734553655b509e2d79e618249548bda050005e8
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-21 10:36:11 +0300

    Initial commit

---
+++ src/Draft_Task1_3_9_0_Whole_Video.java
@@ -0,0 +1,8 @@
+public class Draft_Task1_3_9_0_Whole_Video {
+    public static void main(String[] args) {
+        //  Module1_Tema3_Urok9 Многомерные массивы.
+        //  видео мин 06 40 - внутри круглых скобок нажмите Ctrl + P
+        //  про пакеты здесь: https://www.youtube.com/watch?v=-iEG-0g6CQ0
+        //  закончил видео мин 09 21
+        }
+}
