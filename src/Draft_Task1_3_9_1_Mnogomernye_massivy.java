cfa3fad63c6d21f24aa5c9ce658385417d33ed07
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-21 12:54:36 +0300

    Initial commit

---
+++ src/Draft_Task1_3_9_1_Mnogomernye_massivy.java
@@ -0,0 +1,12 @@
+public class Draft_Task1_3_9_1_Mnogomernye_massivy {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                8. Создайте матрицу char[][], проинициализируйте ее латинским алфавитом,
+                   выведите его. Подсказка - можно получить первую букву как char ch = ‘a’,
+                   следующую - как ch + 1, и так далее.
+                    
+                Решение:\s""");
+
+    }
+}
