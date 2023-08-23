efd6e64a951870bbe5acb4ab28abd5c2c03af954
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-23 16:02:46 +0300

    Initial commit

---
+++ src/com/draft_task1_3_10/Draft_Task1_3_10_1_Pishem_krestiki_noliki.java
@@ -0,0 +1,25 @@
+package com.draft_task1_3_10;
+import java.util.Scanner;
+public class Draft_Task1_3_10_1_Pishem_krestiki_noliki {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                1. Напишите сами крестики-нолики, не подглядывая в наш код.
+
+                Решение:\s""");
+
+
+
+
+
+        }
+}
+
+/*pablic */class FieldX0 {
+    /*public */char[][] field;
+    int size = 3;
+
+    void initField() {
+
+    }
+}
