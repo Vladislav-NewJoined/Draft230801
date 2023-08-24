b8be81553dad503e462c2b7acfdc3a893bae0972
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-24 09:33:19 +0300

    Initial commit

--- src/com/draft_task1_3_10/X0Game.java
+++ src/com/draft_task1_3_10/X0Game.java
@@ -2,9 +2,11 @@ package com.draft_task1_3_10;
 
 import java.util.Scanner;
 
+//pf //  закончил видео мин 11 54
 public class X0Game {
     FieldX0 gameField;
     Scanner scanner = new Scanner (System.in);
+    char whoMakeNextTurn;
 
     void setupNewGame() {
         System.out.println("Will play new game");
@@ -17,6 +19,9 @@ public class X0Game {
 //        String next = this.scanner.next();
 //        char first = next.charAt(0);
         char first = this.scanner.next().charAt(0);
+        if (first == 'X' || first == '0') {
+            this.whoMakeNextTurn = first;
+        }
 
     }
 }
