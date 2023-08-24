6e4914d3ba25c90d55037f1108a12c0f6bdf5a5d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-24 10:35:58 +0300

    Initial commit

--- src/com/draft_task1_3_10/X0Game.java
+++ src/com/draft_task1_3_10/X0Game.java
@@ -21,7 +21,22 @@ public class X0Game {
         char first = this.scanner.next().charAt(0);
         if (first == 'X' || first == '0') {
             this.whoMakeNextTurn = first;
+        } else {
+            System.out.println("I recognizing only X or 0 (zero). So first will be X");
+            this.whoMakeNextTurn = 'X';
         }
+    }
+
+    void turn() {
+        System.out.println(this.whoMakeNextTurn + ", your turn. ");
+        System.out.println("Chose row: ");
+        int rowNumber = this.scanner.nextInt();
+        System.out.println("Chose column: ");
+        int colNumber = this.scanner.nextInt();
+        int rowIndex = rowNumber - 1;
+        int colIndex = colNumber - 1;
+    }
+
 
     }
-}
+
